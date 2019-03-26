efae83ecd386cda24bf63a59c0ba5ee61b5fefa7
MAwais <mawais54013@outlook.com>
2019-03-25 21:50:12 -0700

    added content

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, ThemeProvider, Header } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>App to make video and chat </Text>
+       <View>
+       <Header 
+         centerComponent={{ text: 'Chat-It!', style: { color: '#fff', fontSize: '30'} }}
+       />
+        <Text style={styles.headline}>App to make video and chat </Text>
       </View>
     );
   }
@@ -14,8 +18,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: 30,
+    marginBottom: 30,
     justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  headline: {
+    fontSize: 28,
+    textAlign: 'center',
+    paddingTop: 20,
   },
 });
