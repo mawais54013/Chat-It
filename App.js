383308da45875a7b7867ca669eeeff25c7d239e3
MAwais <mawais54013@outlook.com>
2019-03-25 18:04:57 -0700

    added

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Test App</Text>
+        <Text>App to make video and chat </Text>
       </View>
     );
   }
