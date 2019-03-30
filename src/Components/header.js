21d08488c528bc456ca8179a765a62fb1fcd3565
MAwais <mawais54013@outlook.com>
2019-03-29 22:03:23 -0700

    added google fonts

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,7 +3,7 @@ import './header.css';
 
 class Header extends Component {
     render() {
-      return (<div>Welcome to Chat - It!</div>);
+      return (<div><h1 id="head1">Welcome to Chat - It!</h1></div>);
     }
 }
 export default Header;
