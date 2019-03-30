d15266a0d55900053d18c06fa29817d0d608518f
MAwais <mawais54013@outlook.com>
2019-03-29 17:46:52 -0700

    added content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Header from './Components/header';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <Header/>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Button variant="contained" color="primary">
+      Hello World
+    </Button>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -19,7 +26,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
---
+++ src/Components/header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+    render() {
+      return (<div>Welcome to Chat - It!</div>);
+    }
+}
+export default Header;
