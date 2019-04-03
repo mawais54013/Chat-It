7336fffb5d616609b322e3a272d517a426dd72cc
MAwais <mawais54013@outlook.com>
2019-04-02 19:07:43 -0700

    test success

--- server.js
+++ server.js
@@ -1,47 +1,24 @@
-// const express = require('express');
-// const bodyparser = require('body-parser');
-// const cors = require('cors');
-// const Pusher = require('pusher');
-
-// const app = express();
-
-// const pusher = new Pusher({
-//     appId: '749058',
-//     key: '0309639b3bc0d2427a18',
-//     secret: 'e988bf4661d5c0996a3a',
-//     cluster: 'us3',
-//     useTLS: true
-// });
-
-// app.use(cors());
-// app.use(bodyparser.urlencoded({ extended: false }));
-// app.use(bodyparser.json());
-
-// app.post('/update-editor', (req, res) => {
-//     pusher.trigger('editor', 'text-update', {
-//         ...req.body,
-//     });
-
-//     res.status(200).send('OK');
-// });
-
-// app.set('port', process.env.PORT || 3000);
-// const server = app.listen(app.get('port'), () => {
-//     console.log(`Express running on PORT ${server.address().port}`)
-// });
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
-
-app.listen(process.env.PORT || 8080);
-console.log('server listening on port 3000')
