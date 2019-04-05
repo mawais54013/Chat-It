e7b1f87ef0e86556b499321eddd1c2041792bf60
MAwais <mawais54013@outlook.com>
2019-04-04 21:23:14 -0700

    it works

--- client/src/App.js
+++ client/src/App.js
@@ -57,7 +57,7 @@ import axios from "axios";
 
 import "./App.css";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/material/css";
+import "codemirror/theme/material.css";
 
 import "codemirror/mode/htmlmixed/htmlmixed";
 import "codemirror/mode/css/css";
@@ -70,15 +70,19 @@ class App extends Component {
       id: "",
       html: "",
       css: "",
-      js: ""
+      js: "//code"
     };
 
-    this.pusher = new Pusher("0309639b3bc0d2427a18", {
+    this.pusher = new Pusher("18160601861a89d7f8f7", {
       cluster: "eu",
       forceTLS: true
     });
 
     this.channel = this.pusher.subscribe("editor");
+  }
+
+  componentDidUpdate() {
+    this.runCode();
   }
 
   componentDidMount() {
@@ -88,7 +92,7 @@ class App extends Component {
 
     this.channel.bind("text-update", data => {
       const { id } = this.state;
-      if(data.id === id) return;
+      if (data.id === id) return;
 
       this.setState({
         html: data.html,
@@ -101,10 +105,101 @@ class App extends Component {
   syncUpdates = () => {
     const data = { ...this.state };
 
-    axios 
-        .post("http://localhost:3000/update-editor", data)
-        .catch(console.error);
+    axios
+      .post("http://localhost:5000/update-editor", data)
+      .catch(console.error);
   };
 
-  
+  runCode = () => {
+    const { html, css, js } = this.state;
+    const iframe = this.refs.iframe;
+    const document = iframe.contentDocument;
+    const documentContents = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <style>
+          ${css}
+        </style>
+      </head>
+      <body>
+        ${html}
+        <p id="demo"></p>
+        <script type="text/javascript">
+          ${js}
+        </script>
+      </body>
+      </html>
+    `;
+
+    document.open();
+    document.write(documentContents);
+    document.close();
+  };
+
+  render() {
+    const { html, js, css } = this.state;
+    const codeMirrorOptions = {
+      theme: "material",
+      lineNumbers: true,
+      scrollbarStyle: null,
+      lineWrapping: true
+    };
+
+    return (
+      <div className="App">
+        <section className="playground">
+          {/* <div className="code-editor html-code">
+            <div className="editor-header">HTML</div>
+            <CodeMirror
+              value={html}
+              options={{
+                mode: "htmlmixed",
+                ...codeMirrorOptions
+              }}
+              onBeforeChange={(editor, data, html) => {
+                this.setState({ html }, () => this.syncUpdates());
+              }}
+            />
+
+          </div> */}
+          {/* <div className="code-editor css-code">
+            <div className="editor-header">CSS</div>
+            <CodeMirror
+              value={css}
+              options={{
+                mode: "css",
+                ...codeMirrorOptions
+              }}
+              onBeforeChange={(editor, data, css) => {
+                this.setState({ css }, () => this.syncUpdates());
+              }}
+            />
+          </div> */}
+          <div className="code-editor js-code">
+            <div className="editor-header">JavaScript</div>
+            <CodeMirror
+              value={js}
+              options={{
+                mode: "javascript",
+                ...codeMirrorOptions
+              }}
+              onBeforeChange={(editor, data, js) => {
+                this.setState({ js }, () => this.syncUpdates());
+              }}
+            />
+          </div>
+        </section>
+        <section className="result">
+          <iframe title="result" className="iframe" ref="iframe" />
+        </section>
+      </div>
+    );
+  }
 }
+
+export default App;
--- server.js
+++ server.js
@@ -1,10 +1,54 @@
-const express = require("express");
-const path = require("path");
+// const express = require("express");
+// const path = require("path");
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const Pusher = require('pusher');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+// const pusher = new Pusher({
+//   appId: '749058',
+//   key: '0309639b3bc0d2427a18',
+//   secret: 'e988bf4661d5c0996a3a',
+//   cluster: 'us3',
+//   encrypted: true
+// });
+
+// app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.post('/update-editor', (req, res) => {
+//   pusher.trigger('editor', 'text-update', {
+//     ...req.body,
+//   });
+//   res.status(200).send('OK');
+// });
+// // Define middleware here
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+// // Define API routes here
+
+// // Send every other request to the React app
+// // Define any API routes before this runs
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`🌎 ==> Server now on port ${PORT}!`);
+// });
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const Pusher = require('pusher');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
 
 const pusher = new Pusher({
@@ -12,7 +56,7 @@ const pusher = new Pusher({
   key: '0309639b3bc0d2427a18',
   secret: 'e988bf4661d5c0996a3a',
   cluster: 'us3',
-  encrypted: true
+  useTLS: true,
 });
 
 app.use(cors())
@@ -21,26 +65,13 @@ app.use(bodyParser.json());
 
 app.post('/update-editor', (req, res) => {
   pusher.trigger('editor', 'text-update', {
-    ...req.body,
+   ...req.body,
   });
+
   res.status(200).send('OK');
 });
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+app.set('port', process.env.PORT || 5000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
 });
