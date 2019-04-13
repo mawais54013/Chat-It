61152364fe9007111aa0f3ef103bb5ce6f266bbb
MAwais <mawais54013@outlook.com>
2019-04-12 17:33:17 -0700

    no server,code editor works

--- client/src/pages/Coding.js
+++ client/src/pages/Coding.js
@@ -1,190 +1,19 @@
-// import React, { Component } from "react";
-// import { Controlled as CodeMirror } from "react-codemirror2";
-// import Pusher from "pusher-js";
-// import pushid from "pushid";
-// import axios from "axios";
-// import io from 'socket.io-client'
-
-// // import "./App.css";
-// import "codemirror/lib/codemirror.css";
-// import "codemirror/theme/material.css";
-
-// import "codemirror/mode/htmlmixed/htmlmixed";
-// import "codemirror/mode/css/css";
-// import "codemirror/mode/javascript/javascript";
-
-
-// class CodingPage extends Component {
-//   constructor() {
-
-
-//     super();
-//     this.state = {
-//       id: "",
-//       html: "",
-//       css: "",
-//       js: "",
-//       key1: localStorage.getItem('mainKey'),
-//       key: sessionStorage.getItem("mainKey")
-//     };
-
-//     this.pusher = new Pusher("0309639b3bc0d2427a18", {
-//       cluster: "us3",
-//       forceTLS: false
-//     });
-
-//     this.channel = this.pusher.subscribe(`1`);
-//   }
-
-//   componentDidUpdate() {
-//     this.runCode();
-//   }
-
-//   componentDidMount() {
-//     this.setState({
-//       id: pushid()
-//     });
-
-//     this.channel.bind(`${this.state.key1}`, data => {
-//       const { id } = this.state;
-//       if (data.id === id) return;
-
-//       this.setState({
-//         html: data.html,
-//         css: data.css,
-//         js: data.js,
-//         name: this.state.key
-//       });
-//     });
-//   }
-
-  // syncUpdates = () => {
-  //   const data = { ...this.state };
-
-  //   // axios
-  //   //   .post("http://localhost:5000/update-editor/" + `${this.state.key1}`, data)
-
-  //   //   .then(function (res)
-  //   //   {
-  //   //     console.log(res);
-  //   //   })
-  //   //   .catch(console.error);
-      
-  // };
-
-  // runCode = () => {
-  //   const { html, css, js } = this.state;
-
-  //   const iframe = this.refs.iframe;
-  //   const document = iframe.contentDocument;
-  //   const documentContents = `
-  //     <!DOCTYPE html>
-  //     <html lang="en">
-  //     <head>
-  //       <meta charset="UTF-8">
-  //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  //       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  //       <title>Document</title>
-  //       <style>
-  //         ${css}
-  //       </style>
-  //     </head>
-  //     <body>
-  //       ${html}
-
-  //       <script type="text/javascript">
-  //         ${js}
-  //       </script>
-  //     </body>
-  //     </html>
-  //   `;
-
-  //   document.open();
-  //   document.write(documentContents);
-  //   document.close();
-  // };
-
-  // render() {
-
-  //   console.log(this.state.key1)
-  //   const { html, js, css } = this.state;
-  //   const codeMirrorOptions = {
-  //     theme: "material",
-  //     lineNumbers: true,
-  //     scrollbarStyle: null,
-  //     lineWrapping: true
-  //   };
-
-  //   return (
-  //     <div className="App">
-  //       <section className="playground">
-          // <div className="code-editor html-code">
-          //   <div className="editor-header">HTML</div>
-            // <CodeMirror
-            //   value={html}
-            //   options={{
-            //     mode: "htmlmixed",
-            //     ...codeMirrorOptions
-            //   }}
-            //   onBeforeChange={(editor, data, html) => {
-            //     this.setState({ html }, () => this.syncUpdates());
-            //   }}
-            // />
-          // </div>
-          // <div className="code-editor css-code">
-          //   <div className="editor-header">CSS</div>
-          //   <CodeMirror
-          //     value={css}
-          //     options={{
-          //       mode: "css",
-          //       ...codeMirrorOptions
-          //     }}
-          //     onBeforeChange={(editor, data, css) => {
-          //       this.setState({ css }, () => this.syncUpdates());
-          //     }}
-          //   />
-          // </div>
-          // <div className="code-editor js-code">
-          //   <div className="editor-header">JavaScript</div>
-          //   <CodeMirror
-          //     value={js}
-          //     options={{
-          //       mode: "javascript",
-          //       ...codeMirrorOptions
-          //     }}
-          //     onBeforeChange={(editor, data, js) => {
-          //       this.setState({ js }, () => this.syncUpdates());
-          //     }}
-          //   />
-          // </div>
-  //       </section>
-  //       <section className="result">
-  //         <iframe title="result" className="iframe" ref="iframe" />
-  //       </section>
-  //     </div>
-  //   );
-  // }
-// }
-
-// export default CodingPage;
-
-
-    
 import React from "react";
 import Header from "../Components/header";
 import { database } from "firebase";
 import CodeMirror from "react-codemirror";
 import axios from "axios";
 import "./coding.css";
-// import { Controlled as CodeMirror } from "react-codemirror2";
 
 require("codemirror/lib/codemirror.css");
 require("codemirror/mode/javascript/javascript");
 require("codemirror/theme/dracula.css");
 
 export default class CodingPage extends React.Component {
+
   state = {
     code: "Loading...",
+    code1: `<h1 id='demo'></h1>`,
     cursorPosition: {
       line: 0,
       ch: 0
@@ -236,9 +65,45 @@ export default class CodingPage extends React.Component {
     this.codeRef.child("content").set(newVal);
   };
 
+componentDidUpdate() {
+  this.runCode();
+}
+
+syncUpdates = () => {
+  const data = { ...this.state };    
+};
+
+runCode = () => {
+  const { code1, code } = this.state;
+
+  const iframe = this.refs.iframe;
+  const document = iframe.contentDocument;
+  const documentContents = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+    </head>
+    <body>
+      ${code1}
+
+      <script type="text/javascript">
+        ${code}
+      </script>
+    </body>
+    </html>
+  `;
+
+  document.open();
+  document.write(documentContents);
+  document.close();
+};
 
   render() {
-    const hCode = this.state.code;
+    const { code1, code } = this.state;
     return (
       <React.Fragment>
         <Header
@@ -264,25 +129,28 @@ export default class CodingPage extends React.Component {
               readOnly: false,
               mode: "javascript"
             }}
-          />
-          </div>
-          {/* <div className="codeArea">
-          <CodeMirror
-            ref={r => (this.codemirror = r)}
-            className="code-mirror-container"
-            value={this.state.code}
-            onChange={this.onChange}
-            options={{
-              theme: "dracula",
-              lineNumbers: true,
-              readOnly: false,
-              mode: "js"
+            onBeforeChange={(editor, data, code) => {
+              this.setState({ code }, () => this.syncUpdates());
             }}
           />
-          </div> */}
-          <iframe id="iframe" srcDoc={hCode}>
-          <p>Your browser does not support iframes.</p>
-        </iframe>
+          </div>
+          <div>
+          <CodeMirror
+            value={this.state.code1}
+            options={{
+              mode: 'htmlmixed',
+              theme: 'dracula',
+              lineNumbers: true,
+              readOnly: false,
+            }}
+              onBeforeChange={(editor, data, code1) => {
+                this.setState({ code1 }, () => this.syncUpdates());
+              }}
+          />
+          </div>
+        <section className="result" id="iframe">
+          <iframe title="result" className="iframe" ref="iframe" />
+        </section>
         </div>
       </React.Fragment>
     );
