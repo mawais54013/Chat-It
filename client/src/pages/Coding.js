f5e6cfc3895d7d3ee4159b692b6fa3044f0ec191
MAwais <mawais54013@outlook.com>
2019-04-12 23:16:48 -0700

    added html area

--- client/src/pages/Coding.js
+++ client/src/pages/Coding.js
@@ -1,17 +1,132 @@
+// import React, { Component } from "react";
+// // import { Controlled as CodeMirror } from "react-codemirror2";
+// import CodeMirror from "react-codemirror";
+// import Pusher from "pusher-js";
+// import pushid from "pushid";
+// import axios from "axios";
+// // import io from 'socket.io-client'
+
+// // import "./App.css";
+// import "codemirror/lib/codemirror.css";
+// import "codemirror/theme/material.css";
+
+// import "codemirror/mode/htmlmixed/htmlmixed";
+// import "codemirror/mode/css/css";
+// import "codemirror/mode/javascript/javascript";
+
+
+// class CodingPage extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       html: "",
+//       js: "",
+//     };
+//   }
+
+//   componentDidUpdate() {
+//     this.runCode();
+//   }
+
+//   syncUpdates = () => {
+//     const data = { ...this.state };    
+//   };
+
+//   runCode = () => {
+//     const { html, js } = this.state;
+
+//     const iframe = this.refs.iframe;
+//     const document = iframe.contentDocument;
+//     const documentContents = `
+//       <!DOCTYPE html>
+//       <html lang="en">
+//       <head>
+//         <meta charset="UTF-8">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//         <title>Document</title>
+//       </head>
+//       <body>
+//         ${html}
+
+//         <script type="text/javascript">
+//           ${js}
+//         </script>
+//       </body>
+//       </html>
+//     `;
+
+//     document.open();
+//     document.write(documentContents);
+//     document.close();
+//   };
+
+//   render() {
+
+//     const { html, js } = this.state;
+//     const codeMirrorOptions = {
+//       theme: "material",
+//       lineNumbers: true,
+//       scrollbarStyle: null,
+//       lineWrapping: true
+//     };
+
+//     return (
+//       <div className="App">
+//         <section className="playground">
+//           <div className="code-editor html-code">
+//             <div className="editor-header">HTML</div>
+//             <CodeMirror
+//               value={html}
+//               options={{
+//                 mode: "htmlmixed",
+//                 ...codeMirrorOptions
+//               }}
+//               onBeforeChange={(editor, data, html) => {
+//                 this.setState({ html }, () => this.syncUpdates());
+//               }}
+//             />
+//           </div>
+//           <div className="code-editor js-code">
+//             <div className="editor-header">JavaScript</div>
+//             <CodeMirror
+//               value={js}
+//               options={{
+//                 mode: "javascript",
+//                 ...codeMirrorOptions
+//               }}
+//               onBeforeChange={(editor, data, js) => {
+//                 this.setState({ js }, () => this.syncUpdates());
+//               }}
+//             />
+//           </div>
+//         </section>
+//         <section className="result">
+//           <iframe title="result" className="iframe" ref="iframe" />
+//         </section>
+//       </div>
+//     );
+//   }
+// }
+
+// export default CodingPage;
+
+
+    
 import React from "react";
 import Header from "../Components/header";
 import { database } from "firebase";
 import CodeMirror from "react-codemirror";
-import axios from "axios";
 import "./coding.css";
 
 require("codemirror/lib/codemirror.css");
+require("codemirror/mode/htmlmixed/htmlmixed");
 require("codemirror/mode/javascript/javascript");
 require("codemirror/theme/dracula.css");
 
 export default class CodingPage extends React.Component {
-
   state = {
+    html: '',
     code: "Loading...",
     code1: `<h1 id='demo'></h1>`,
     cursorPosition: {
@@ -19,6 +134,7 @@ export default class CodingPage extends React.Component {
       ch: 0
     }
   };
+
   componentDidMount = () => {
 
     const { params } = this.props.match;
@@ -65,16 +181,19 @@ export default class CodingPage extends React.Component {
     this.codeRef.child("content").set(newVal);
   };
 
+onChange1(data) {
+  this.setState({
+    code1: data,
+  })
+  this.runCode();
+}
+
 componentDidUpdate() {
   this.runCode();
 }
 
-syncUpdates = () => {
-  const data = { ...this.state };    
-};
-
 runCode = () => {
-  const { code1, code } = this.state;
+  const { code, code1 } = this.state;
 
   const iframe = this.refs.iframe;
   const document = iframe.contentDocument;
@@ -103,7 +222,8 @@ runCode = () => {
 };
 
   render() {
-    const { code1, code } = this.state;
+    const { html } = this.state;
+
     return (
       <React.Fragment>
         <Header
@@ -122,6 +242,7 @@ runCode = () => {
             ref={r => (this.codemirror = r)}
             className="code-mirror-container"
             value={this.state.code}
+            // value={js}
             onChange={this.onChange}
             options={{
               theme: "dracula",
@@ -129,23 +250,20 @@ runCode = () => {
               readOnly: false,
               mode: "javascript"
             }}
-            onBeforeChange={(editor, data, code) => {
-              this.setState({ code }, () => this.syncUpdates());
-            }}
+
           />
           </div>
           <div>
           <CodeMirror
             value={this.state.code1}
+            onChange={value => { this.onChange1(value) }}
+            // value={html}
             options={{
               mode: 'htmlmixed',
               theme: 'dracula',
               lineNumbers: true,
               readOnly: false,
             }}
-              onBeforeChange={(editor, data, code1) => {
-                this.setState({ code1 }, () => this.syncUpdates());
-              }}
           />
           </div>
         <section className="result" id="iframe">
