62f8e8a171c9db6f001e6cadb8b79fccbc8f4145
MAwais <mawais54013@outlook.com>
2019-04-17 23:18:28 -0700

    webrtc works and added

--- client/src/App.js
+++ client/src/App.js
@@ -59,106 +59,106 @@ export default App;
 //     if (iOSSafari) this.setState({ iOS: true });
 //   }
 
-//   handleStart = () => {
-//     if (!this.state.nick || !this.state.roomName) {
-//       alert('Please enter all fields.');
-//       return;
-//     }
-//     this.setState({ startChat: true });
-//     window.location.hash = `#${this.state.roomName}`;
-//   }
+  // handleStart = () => {
+  //   if (!this.state.nick || !this.state.roomName) {
+  //     alert('Please enter all fields.');
+  //     return;
+  //   }
+  //   this.setState({ startChat: true });
+  //   window.location.hash = `#${this.state.roomName}`;
+  // }
 
-//   handleShareDialogClose = () => this.setState({ sharing: false });
+  // handleShareDialogClose = () => this.setState({ sharing: false });
 
-//   handleInvite = () => this.setState({ sharing: true });
+  // handleInvite = () => this.setState({ sharing: true });
 
-//   handleQuit = () => this.setState({ startChat: false });
+  // handleQuit = () => this.setState({ startChat: false });
 
-//   handleChangeColor = () => this.setState({ choosingColor: !this.state.choosingColor });
+  // handleChangeColor = () => this.setState({ choosingColor: !this.state.choosingColor });
 
-//   handleStartVideo = () => this.setState({ iOS: false });
+  // handleStartVideo = () => this.setState({ iOS: false });
 
-//   handleColorSlide = (color) => this.setState({ windowColor: color.rgb });
+  // handleColorSlide = (color) => this.setState({ windowColor: color.rgb });
 
 //   render() {
 //     return (
 //       <React.Fragment>
-//         <div className="App">
-//           <div className="header">
-//             <h1>P2P Video Chat Demo</h1>
-//             <p>To try this out, open this page in another tab, or send this page to some friends. Open dev tools to see the logging.
-//               To view the source code for this app, <a href="https://github.com/lazorfuzz/liowebrtc-video-demo" target="_blank" rel="noopener noreferrer">click here</a>. This app is powered by <a href="https://github.com/lazorfuzz/liowebrtc" target="_blank" rel="noopener noreferrer">LioWebRTC</a>.
-//               <br /><br />
-//               <a className="github-button" href="https://github.com/lazorfuzz/liowebrtc" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star lazorfuzz/liowebrtc on GitHub">Star LioWebRTC on GitHub</a></p>
-//           </div>
-//           {
-//             !this.state.startChat &&
-//             <div className="setup">
-//               <input
-//                 className="textBox"
-//                 type="text"
-//                 placeholder="Room Name"
-//                 value={this.state.roomName}
-//                 ref={(el) => this.roomNameInput = el}
-//                 onChange={(e) => { this.setState({ roomName: e.target.value }) }}
-//               />
-//               <input
-//                 className="textBox"
-//                 type="text"
-//                 placeholder="Nickname"
-//                 value={this.state.nick}
-//                 ref={(el) => this.nickInput = el}
-//                 onChange={(e) => { this.setState({ nick: e.target.value }) }}
-//                 onKeyUp={(e) => {
-//                   if (e.key === 'Enter') {
-//                     this.handleStart();
-//                   }
-//                 }}
-//               />
-//               <button
-//                 className="btn"
-//                 onClick={this.handleStart}
-//                 >Enter</button>
-//             </div>
-//           }
-//           {
-//             this.state.startChat &&
-//             <div className="partyWrapper">
-//               <div className="controls">
-//                 <p className="roomName">Room Name: <span className="roomLabel">{`${this.state.roomName}`}</span></p>
-//                 <div className="menu">
-//                   {
-//                     this.state.choosingColor &&
-//                     <div className="colorPopOver">
-//                       <div className="colorCover" onClick={() => this.setState({ choosingColor: false })} />
-//                     </div>
-//                   }
-//                   <button
-//                     iconStyle={{ color: 'white' }}
-//                     tooltip="Add Friends"
-//                     onClick={this.handleInvite}
-//                     >
-//                       <i className="material-icons">group_add</i>
-//                   </button>
-//                   <button
-//                     iconStyle={{ color: 'white' }}
-//                     tooltip="Quit"
-//                     onClick={this.handleQuit}
-//                     >
-//                       <i className="material-icons">power_settings_new</i>
-//                   </button>
-//                 </div>
-//               </div>
-//               <Party
-//                 roomName={`liowebrtc-vchat-demo-${this.state.roomName}`}
-//                 nick={this.state.nick}
-//                 windowColor={this.state.windowColor}
-//                 iOS={this.state.iOS}
-//                 handleStartVideo={this.handleStartVideo}
-//               />
-//             </div>
-//           }
-//         </div>
+        // <div className="App">
+        //   <div className="header">
+        //     <h1>P2P Video Chat Demo</h1>
+        //     <p>To try this out, open this page in another tab, or send this page to some friends. Open dev tools to see the logging.
+        //       To view the source code for this app, <a href="https://github.com/lazorfuzz/liowebrtc-video-demo" target="_blank" rel="noopener noreferrer">click here</a>. This app is powered by <a href="https://github.com/lazorfuzz/liowebrtc" target="_blank" rel="noopener noreferrer">LioWebRTC</a>.
+        //       <br /><br />
+        //       <a className="github-button" href="https://github.com/lazorfuzz/liowebrtc" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star lazorfuzz/liowebrtc on GitHub">Star LioWebRTC on GitHub</a></p>
+        //   </div>
+        //   {
+        //     !this.state.startChat &&
+        //     <div className="setup">
+        //       <input
+        //         className="textBox"
+        //         type="text"
+        //         placeholder="Room Name"
+        //         value={this.state.roomName}
+        //         ref={(el) => this.roomNameInput = el}
+        //         onChange={(e) => { this.setState({ roomName: e.target.value }) }}
+        //       />
+        //       <input
+        //         className="textBox"
+        //         type="text"
+        //         placeholder="Nickname"
+        //         value={this.state.nick}
+        //         ref={(el) => this.nickInput = el}
+        //         onChange={(e) => { this.setState({ nick: e.target.value }) }}
+        //         onKeyUp={(e) => {
+        //           if (e.key === 'Enter') {
+        //             this.handleStart();
+        //           }
+        //         }}
+        //       />
+        //       <button
+        //         className="btn"
+        //         onClick={this.handleStart}
+        //         >Enter</button>
+        //     </div>
+        //   }
+        //   {
+        //     this.state.startChat &&
+        //     <div className="partyWrapper">
+        //       <div className="controls">
+        //         <p className="roomName">Room Name: <span className="roomLabel">{`${this.state.roomName}`}</span></p>
+        //         <div className="menu">
+        //           {
+        //             this.state.choosingColor &&
+        //             <div className="colorPopOver">
+        //               <div className="colorCover" onClick={() => this.setState({ choosingColor: false })} />
+        //             </div>
+        //           }
+        //           <button
+        //             iconStyle={{ color: 'white' }}
+        //             tooltip="Add Friends"
+        //             onClick={this.handleInvite}
+        //             >
+        //               <i className="material-icons">group_add</i>
+        //           </button>
+        //           <button
+        //             iconStyle={{ color: 'white' }}
+        //             tooltip="Quit"
+        //             onClick={this.handleQuit}
+        //             >
+        //               <i className="material-icons">power_settings_new</i>
+        //           </button>
+        //         </div>
+        //       </div>
+        //       <Party
+        //         roomName={`liowebrtc-vchat-demo-${this.state.roomName}`}
+        //         nick={this.state.nick}
+        //         windowColor={this.state.windowColor}
+        //         iOS={this.state.iOS}
+        //         handleStartVideo={this.handleStartVideo}
+        //       />
+        //     </div>
+        //   }
+        // </div>
 //       </React.Fragment>
 //     );
 //   }
--- client/src/pages/Coding.js
+++ client/src/pages/Coding.js
@@ -119,6 +119,7 @@ import Header from "../Components/header";
 import { database } from "firebase";
 import CodeMirror from "react-codemirror";
 import Party from '../Components/Party'
+import { getRandomRGB } from '../Components/util';
 import "./coding.css";
 
 require("codemirror/lib/codemirror.css");
@@ -135,10 +136,19 @@ export default class CodingPage extends React.Component {
       line: 0,
       ch: 0
     }, 
+    startChat: false,
+    nick: '',
+    roomName: '',
+    sharing: false,
+    windowColor: getRandomRGB(),
+    iOS: false,
+    startVideo: false,
+    chooseColor: false,
   };
 
   componentDidMount = () => {
-    // console.log(localStorage.getItem("room"))
+    this.setUp();
+
     const { params } = this.props.match;
     let self = this;
     database()
@@ -223,6 +233,44 @@ runCode = () => {
   document.close();
 };
 
+setUp = () => {
+  this.checkiOS();
+  if(window.innerWidth > 800) this.roomNameInput.focus();
+  if(window.location.hash && window.location.hash.length > 1) {
+    this.setState({ roomName: window.location.hash.slice(1) });
+    if(window.innerWidth > 800) this.nickInput.focus();
+  }
+}
+
+checkiOS = () => {
+  const ua = window.navigator.userAgent;
+  const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+  const webkit = !!ua.match(/Webkit/i);
+  const iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
+  if(iOSSafari) this.setState({ iOS: true });
+}
+
+handleStart = () => {
+  if (!this.state.nick || !this.state.roomName) {
+    alert('Please enter all fields.');
+    return;
+  }
+  this.setState({ startChat: true });
+  window.location.hash = `#${this.state.roomName}`;
+}
+
+handleShareDialogClose = () => this.setState({ sharing: false });
+
+handleInvite = () => this.setState({ sharing: true });
+
+handleQuit = () => this.setState({ startChat: false });
+
+handleChangeColor = () => this.setState({ choosingColor: !this.state.choosingColor });
+
+handleStartVideo = () => this.setState({ iOS: false });
+
+handleColorSlide = (color) => this.setState({ windowColor: color.rgb });
+
   render() {
     const { html } = this.state;
 
@@ -273,8 +321,81 @@ runCode = () => {
         </section>
         </div>
           
-        <div>
-
+        <div className="App">
+          <div className="header">
+            <h1>P2P Video Chat Demo</h1>
+            <p>To try this out, open this page in another tab, or send this page to some friends. Open dev tools to see the logging.
+              To view the source code for this app, <a href="https://github.com/lazorfuzz/liowebrtc-video-demo" target="_blank" rel="noopener noreferrer">click here</a>. This app is powered by <a href="https://github.com/lazorfuzz/liowebrtc" target="_blank" rel="noopener noreferrer">LioWebRTC</a>.
+              <br /><br />
+              <a className="github-button" href="https://github.com/lazorfuzz/liowebrtc" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star lazorfuzz/liowebrtc on GitHub">Star LioWebRTC on GitHub</a></p>
+          </div>
+          {
+            !this.state.startChat &&
+            <div className="setup">
+              <input
+                className="textBox"
+                type="text"
+                placeholder="Room Name"
+                value={this.state.roomName}
+                ref={(el) => this.roomNameInput = el}
+                onChange={(e) => { this.setState({ roomName: e.target.value }) }}
+              />
+              <input
+                className="textBox"
+                type="text"
+                placeholder="Nickname"
+                value={this.state.nick}
+                ref={(el) => this.nickInput = el}
+                onChange={(e) => { this.setState({ nick: e.target.value }) }}
+                onKeyUp={(e) => {
+                  if (e.key === 'Enter') {
+                    this.handleStart();
+                  }
+                }}
+              />
+              <button
+                className="btn"
+                onClick={this.handleStart}
+                >Enter</button>
+            </div>
+          }
+          {
+            this.state.startChat &&
+            <div className="partyWrapper">
+              <div className="controls">
+                <p className="roomName">Room Name: <span className="roomLabel">{`${this.state.roomName}`}</span></p>
+                <div className="menu">
+                  {
+                    this.state.choosingColor &&
+                    <div className="colorPopOver">
+                      <div className="colorCover" onClick={() => this.setState({ choosingColor: false })} />
+                    </div>
+                  }
+                  <button
+                    iconStyle={{ color: 'white' }}
+                    tooltip="Add Friends"
+                    onClick={this.handleInvite}
+                    >
+                      <i className="material-icons">group_add</i>
+                  </button>
+                  <button
+                    iconStyle={{ color: 'white' }}
+                    tooltip="Quit"
+                    onClick={this.handleQuit}
+                    >
+                      <i className="material-icons">power_settings_new</i>
+                  </button>
+                </div>
+              </div>
+              <Party
+                roomName={`liowebrtc-vchat-demo-${this.state.roomName}`}
+                nick={this.state.nick}
+                windowColor={this.state.windowColor}
+                iOS={this.state.iOS}
+                handleStartVideo={this.handleStartVideo}
+              />
+            </div>
+          }
         </div>
       </React.Fragment>
     );
